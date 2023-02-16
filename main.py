6fa2e6c2270d9bfe12d0dd881927e67c7c877a2e
Hafsa Yasin <hafsayasin2017@gmail.com>
2023-02-16 04:13:23 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def class_strength():
+  Total_student =input("Enter total student")
+  Bio_student =input("Enter bio student")
+  Math_student =input("Enter math student")
+  print(int(Bio_student)+ int(Math_student))
+  print(int(Total_student)+int(Bio_student)+ int(Math_student))
+class_strength()
